8e7f2778f0bc2b5ca10338604c52445c3f2194cf
ibrahimiltifat <59937739+ibrahimiltifat@users.noreply.github.com>
2021-01-17 21:17:32 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,73 +1,101 @@
-import pygame
-from grid import Grid
 
-surface = pygame.display.set_mode((600, 600))
-pygame.display.set_caption('TIC_TAC_TOE')
-
-import threading
-
-
-def create_thread(target):
-    thread = threading.Thread(target=target)
-    thread.daemon = True
-    thread.start()
-
-
-# creating a TCP socket for the server
+import tkinter
 import socket
+import threading
+import time
 
-HOST = '127.0.0.1'
-PORT = 65004
-connection_established = False
-conn, addr = None, None
+server = None
+ADDR = "127.0.0.1"
+PORT = 8012
+name = ""
+allclients = []
+clientnames = []
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((HOST, PORT))
-sock.listen(1)
+#setting up the GUI
+whole_Frame = tkinter.Tk()
+whole_Frame.title("CS447-Project")
 
-player = "X"
+showclients = tkinter.Frame(whole_Frame)
+displayer = tkinter.Text(showclients, height=20, width=25)
+displayer.pack(side=tkinter.RIGHT, fill=tkinter.X, pady=(5, 0))
+displayer.config(background="#A2A3A4", state="disabled")
+showclients.pack(side=tkinter.TOP)
 
-
-def recieve_data():
-
-    while True:
-        data = conn.recv(1024).decode()
-        data = data.split('-')
-        x, y, player = int(data[0]), int(data[1]), data[2]
-        grid.set_cell_value(x,y,player)
-
-
-def waiting_for_connection():
-    global connection_established, conn, addr
-    conn, addr = sock.accept()  # wait for a connection, it is a blocking method
-    print('client is connected')
-    connection_established = True
-    recieve_data()
+buttonCol = tkinter.Frame(whole_Frame)
+serverStarter = tkinter.Button(buttonCol, text="Run Server", command=lambda: runserver())
+serverStarter.pack(side=tkinter.RIGHT)
+buttonCol.pack(side=tkinter.BOTTOM)
 
 
-# run the blocking functions in a separate thread
+def runserver():
+    serverStarter.config(state=tkinter.DISABLED)
+    global server
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((ADDR, PORT))
+    server.listen(5)
+    th = threading.Thread(target=letclientconnect, args=(server, ""))
+    th.setDaemon(True)
+    th.start()
 
-create_thread(waiting_for_connection)
 
-grid = Grid()
-running = True
-grid.draw(surface)
+def letclientconnect(srvr, st):
+    while 1:
+        if len(allclients) < 2:
+            cli, addr = srvr.accept()
+            allclients.append(cli)
+            th = threading.Thread(target=communicate, args=(cli, addr))
+            th.setDaemon(True)
+            th.start()
 
-while running:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if pygame.mouse.get_pressed():
-                pos = pygame.mouse.get_pos()
-                cellX, cellY = pos[0] // 200, pos[1] // 200
-                grid.get_mouse(cellX, cellY, player)
-                send_data = '{}-{}-{}'.format(cellX, cellY, player).encode()
-                conn.send(send_data)
-                if grid.winning(player):
-                    running = False
+def communicate(cli, address):
+    global server, name, allclients
+    name = cli.recv(4096).decode()
+    if len(allclients) < 2:
+        cli.send("w1".encode())#assigning client1 and client2
+    else:
+        cli.send("w2".encode())
 
-    surface.fill((0, 0, 0))
-    grid.draw(surface)
-    pygame.display.flip()
+    clientnames.append(name)
+    updateDisplayer(clientnames)
+
+    if len(allclients) > 1:
+        time.sleep(1)
+        allclients[0].send(("rvl" + clientnames[1] + "X").encode())#exchigng name information with the clients
+        allclients[1].send(("rvl" + clientnames[0] + "O").encode())
+
+    while 1:
+        try:
+            rcvd = cli.recv(4096).decode()
+            if rcvd is None:
+                break
+            elif rcvd.startswith("|cor"):
+                if cli == allclients[0]:#telling both the clients about new coordinates after every turn
+                    allclients[1].send(rcvd.encode())
+                elif cli == allclients[1]:
+                    allclients[0].send(rcvd.encode())
+        except Exception as w:
+            break
+    index = indexofcli(allclients, cli)
+    print(index)
+    del clientnames[index], allclients[index]
+    cli.close()
+    updateDisplayer(clientnames)
+
+
+def indexofcli(all, searched):
+    for clis in range(len(all)):
+        if all[clis] == searched:
+            return clis
+    return -1
+
+
+def updateDisplayer(clinames):
+    displayer.config(state=tkinter.NORMAL)
+    displayer.delete("1.0", tkinter.END)
+    for name in clinames:
+        displayer.insert(tkinter.END, name + "\n")
+    displayer.config(state=tkinter.DISABLED)
+
+
+whole_Frame.mainloop()
